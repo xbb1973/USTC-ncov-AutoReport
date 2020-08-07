f881354f6cae3527a022ba54bd9567d8509859f6
BwZhang <36587493+ForeverFancy@users.noreply.github.com>
2020-08-07 08:32:02 +0800

    Update Autoreport.py

--- Autoreport.py
+++ Autoreport.py
@@ -26,55 +26,55 @@ class AutoRepot(object):
     def report(self):
         session = self.login()
         cookies = session.cookies
-        try:
-            data = session.get(
-                "http://weixine.ustc.edu.cn/2020").text
-            data = data.encode('ascii','ignore').decode('utf-8','ignore')
-            soup = BeautifulSoup(data, 'lxml')
-            token = soup.find("input", {"name": "_token"})['value']
-            
-            print("begin report...")
-            
-            with open(self.data_path, "r+") as f:
-                data = f.read()
-                data = json.loads(data)
-                data["_token"]=token
+#         try:
+        data = session.get(
+            "http://weixine.ustc.edu.cn/2020").text
+        data = data.encode('ascii','ignore').decode('utf-8','ignore')
+        soup = BeautifulSoup(data, 'lxml')
+        token = soup.find("input", {"name": "_token"})['value']
 
-            headers = {
-                'authority': 'weixine.ustc.edu.cn',
-                'origin': 'http://weixine.ustc.edu.cn',
-                'upgrade-insecure-requests': '1',
-                'content-type': 'application/x-www-form-urlencoded',
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36',
-                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'referer': 'http://weixine.ustc.edu.cn/2020/',
-                'accept-language': 'zh-CN,zh;q=0.9',
-                'Connection': 'close',
-                'cookie': 'PHPSESSID=' + cookies.get("PHPSESSID") + ";XSRF-TOKEN=" + cookies.get("XSRF-TOKEN") + ";laravel_session="+cookies.get("laravel_session"),
-            }
+        print("begin report...")
 
-            url = "http://weixine.ustc.edu.cn/2020/daliy_report"
-            session.post(url, data=data, headers=headers)
-            data = session.get("http://weixine.ustc.edu.cn/2020").text
-            soup = BeautifulSoup(data, 'lxml')
-            pattern = re.compile("2020-0[0-9]-[0-9]{2}")
-            token = soup.find(
-                "span", {"style": "position: relative; top: 5px; color: #666;"})
-            flag = False
-            if pattern.search(token.text) is not None:
-                date = pattern.search(token.text).group()
-                flag = (time.strftime("%Y-%m-%d", time.localtime()) == date)
-            if flag == False:
-                print("Failed report.")
-            else:
-                print("Successful report.")
-            #######################
-            # self.send_mail(flag)
-            #######################
-            print("end report...")
+        with open(self.data_path, "r+") as f:
+            data = f.read()
+            data = json.loads(data)
+            data["_token"]=token
 
-        except ValueError:
-            print("Error...")
+        headers = {
+            'authority': 'weixine.ustc.edu.cn',
+            'origin': 'http://weixine.ustc.edu.cn',
+            'upgrade-insecure-requests': '1',
+            'content-type': 'application/x-www-form-urlencoded',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'referer': 'http://weixine.ustc.edu.cn/2020/',
+            'accept-language': 'zh-CN,zh;q=0.9',
+            'Connection': 'close',
+            'cookie': 'PHPSESSID=' + cookies.get("PHPSESSID") + ";XSRF-TOKEN=" + cookies.get("XSRF-TOKEN") + ";laravel_session="+cookies.get("laravel_session"),
+        }
+
+        url = "http://weixine.ustc.edu.cn/2020/daliy_report"
+        session.post(url, data=data, headers=headers)
+        data = session.get("http://weixine.ustc.edu.cn/2020").text
+        soup = BeautifulSoup(data, 'lxml')
+        pattern = re.compile("2020-0[0-9]-[0-9]{2}")
+        token = soup.find(
+            "span", {"style": "position: relative; top: 5px; color: #666;"})
+        flag = False
+        if pattern.search(token.text) is not None:
+            date = pattern.search(token.text).group()
+            flag = (time.strftime("%Y-%m-%d", time.localtime()) == date)
+        if flag == False:
+            print("Failed report.")
+        else:
+            print("Successful report.")
+        #######################
+        # self.send_mail(flag)
+        #######################
+        print("end report...")
+
+#         except ValueError:
+#             print("Error...")
             ######################
             # self.send_mail(False)
             ######################
